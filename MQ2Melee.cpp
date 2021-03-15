dccb808a4effded6b5f79993179f22ecc76de4f3
Knightly <knightly.cavalier@gmail.com>
2021-03-14 14:24:46 -1000

    Fixes for Next, cleanup isAAPurchased

--- MQ2Melee.cpp
+++ MQ2Melee.cpp
@@ -3352,42 +3352,15 @@ void BashPress() {
     if (savedpri) Equip(savedpri, InvSlot_Primary);
 }
 
-bool isAAPurchased(PCHAR AAName) {
-    DWORD level = -1;
-    if (PSPAWNINFO pMe = (PSPAWNINFO)pLocalPlayer) {
-        level = pMe->Level;
-    }
-
-    for (unsigned long nAbility = 0; nAbility < AA_CHAR_MAX_REAL; nAbility++) {
-        PALTABILITY pAbility = GetAAByIdWrapper(pPCData->GetAlternateAbilityId(nAbility), level);
-
-        //test for good structure, and level as quick fail fast, the above wrapper not use level on EMU
-        if ( pAbility && pAbility->MinLevel <= level ) {
-            PCHAR pName = pCDBStr->GetString(pAbility->nName, 1, NULL);
-            if (pName && !_stricmp(AAName, pName)) {
-            // good level,  good name,   return postive find.
-                return true;
-            }
-        }
-	}
-    // failed to find by level and name checks,  return negative find.
-    return false;
-}
-
-bool is2HBashAAPurchased(long playerClass) {
-    //fail fast for non-2HB AA classes
-    if (playerClass != EQData::Warrior && playerClass != EQData::Paladin && playerClass != EQData::Shadowknight)
+bool isAAPurchased(const char* AAName) {
+    if (!pLocalPlayer)
         return false;
 
-#if !defined(ROF2EMU) && !defined(UFEMU)
-    if (playerClass == EQData::Warrior)
-        return isAAPurchased("Two-Handed Bash");
-    if (playerClass == EQData::Paladin || playerClass == EQData::Shadowknight)
-        return isAAPurchased("Improved Bash");
+    if(const ALTABILITY* pAltAbility = GetAAByIdWrapper(GetAAIndexByName(AAName), pLocalPlayer->Level))
+    {
+        return pAltAbility->CurrentRank > 0;
+    }
     return false;
-#else
-    return isAAPurchased("2 Hand Bash");
-#endif
 }
 
 void Configure() {
@@ -3414,7 +3387,14 @@ void Configure() {
         }
     }
 
-    HaveBash = is2HBashAAPurchased(Class);
+    if (Class == Warrior)
+    {
+        HaveBash = isAAPurchased("Two-Handed Bash");
+    }
+    else if (Class == Paladin || Class == Shadowknight)
+    {
+        HaveBash = isAAPurchased("Improved Bash");
+    }
 
     BardClass = false;
     BerserkerClass = false;
